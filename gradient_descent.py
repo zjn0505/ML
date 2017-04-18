5461746fcfb1d846a0ffd7cb2df7ef4ae1f35c30
zjn0505 <zjn0505@hotmail.com>
2017-04-18 20:19:47 +0800

    Try using Matrix Vector multiplication for calc

--- gradient_descent.py
+++ gradient_descent.py
@@ -44,5 +44,11 @@ plt.scatter(x, y, s=20, c='b', marker="o")
 y_predict = []
 for i in range(len(x)):
     y_predict.append(theta0 + theta1 * x[i])
-plt.plot(x, y_predict)
+#plt.plot(x, y_predict)
+
+# OR using Matrix Vector Multiplication to for calculation
+dataMatrix = np.stack((np.ones(m),x), axis = -1)
+parameters = [theta0, theta1]
+prediction = dataMatrix.dot(parameters)
+plt.plot(x, prediction)
 plt.show()

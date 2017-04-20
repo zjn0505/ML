0e2ab5b91b960a30115e13fcf01a98c716d3fc3c
Jienan Zhang <zjn0505@hotmail.com>
2017-04-20 20:01:04 +0800

    Add folder structure

--- Python/gradient_descent.py
+++ Python/gradient_descent.py
@@ -50,7 +50,7 @@ for i in range(len(x)):
     y_predict.append(theta0 + theta1 * x[i])
 # plt.plot(x, y_predict)
 
-# OR using Matrix Vector Multiplication to for calculation
+# OR using Matrix Vector Multiplication for calculation
 dataMatrix = np.stack((np.ones(m), x), axis=-1)
 parameters = [theta0, theta1]
 prediction = dataMatrix.dot(parameters)
